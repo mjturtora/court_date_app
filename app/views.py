0884a72aa4af85098d38957c8fa120849d4d511b
mjturtora <mjturtora@gmail.com>
2016-06-18 04:37:41 -0400

    Added input form

--- app/views.py
+++ app/views.py
@@ -1,9 +1,16 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import InputForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html',
-                           title='Home',
-                           )
+                           title='Home')
+
+@app.route('/input', methods=['GET', 'POST'])
+def input():
+    form = InputForm()
+    return render_template('input.html',
+                           title='Input Name',
+                           form=form)
---
+++ config.py
@@ -0,0 +1,2 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'not-much-of-a-secret'
