fa012defd5630422f0a6d7b5609d9b6471931908
mjturtora <mjturtora@gmail.com>
2016-06-21 03:46:48 -0400

    added error handling 404 and 500

--- app/views.py
+++ app/views.py
@@ -28,3 +28,13 @@ def results():
                            title='My Court Date Is',
                            )
 
+
+# http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-vii-unit-testing
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
