86becf66dca2868384564632532781e32ff16caf
mjturtora <mjturtora@gmail.com>
2016-06-22 20:57:28 -0400

    Improved form validation and re-added 404 and 500 html.

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,15 @@ class InputForm(Form):
     last_name = StringField('last_name', validators=[DataRequired()])
     case_num = StringField('case_num', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
+
+    def validate(self):
+        if not Form.validate(self):
+            print "not form validate"
+            if self.first_name.data or \
+                    self.last_name.data or \
+                    self.case_num.data:
+                print "Should return True"
+                return True
+        print "Should return False"
+
+        return False
