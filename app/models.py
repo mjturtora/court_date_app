efb1802f944c770ad1da967344ff462f76f88133
mjturtora <mjturtora@gmail.com>
2016-06-27 10:59:49 -0400

    returned search prints

--- app/models.py
+++ app/models.py
@@ -20,16 +20,16 @@ def search_all(first, last, case):
         df = pd.read_sql_query(sql_text, engine)
         #print 'len(df) = ', len(df)
     elif first:
-        print 'Searching for first'
+        print 'Searching for first = ', first
         #print 'Only first name entered, searching for first name= ', first
         sql_text = 'SELECT * FROM odyssey WHERE firstname_stripped LIKE "%' + first + '%";'
         df = pd.read_sql_query(sql_text, engine)
         #print 'len(df) = ', len(df)
         #print 'After first name search, df= ', df
     if len(df) > 1:
-        #print 'Searching for first after initial search.'
         #print 'len(df) = ', len(df)
         if first:
+            print 'Searching for first after initial search. First = ', first
             df = df.loc[df['firstname_stripped'].str.contains(first)]
     if len(df) == 0:
         print "Need to add NO RESULTS prompt and return to index"
