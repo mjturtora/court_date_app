798e7d24a7cc727bcb2fbdfe9f40e58196ad921c
mjturtora <mjturtora@gmail.com>
2016-08-02 02:27:42 -0400

    py.test path resolved

--- test/test_utilities.py
+++ test/test_utilities.py
@@ -1,12 +1,15 @@
 from context import utilities
 from utilities import paramiko_sftp as ps
+import os
 
-
+print '\nIn test'
+"""
 def test_ptdummy_t():
     assert ps.pt_dummy(2) == 3
 
 def test_ptdummy_f():
     assert ps.pt_dummy(2) == 4
+"""
 
 def test_readauth():
     assert len(ps.read_auth()) == 4
